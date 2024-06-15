1c5c307c8e5722776b6593554a8921476d6b8632
Cruz Lopez <cml231@txstate.edu>
2024-06-15 14:23:08 -0500

    started refactor

--- Scripts/Ball.cs
+++ Scripts/Ball.cs
@@ -1,34 +1,77 @@
 using Godot;
 using System;
 
-public partial class Ball : Area2D
+public partial class Ball : CharacterBody2D
 {
-    public float Speed = 400f;
-    public Vector2 direction = Vector2.Left;
 
-    private Vector2 _velocity = new Vector2();
+	public Vector2 win_size;
+	public const float StartSpeed = 500f;
+	public const float Accel = 50f;
+	public float speed;
+	public Vector2 dir;
+	public const float MaxYVector = 0.6f;
+
+
+    public override void _Ready()
+    {
+		win_size = GetViewportRect().Size;
+		GD.Randomize();
+		NewBall();
+    }
+
+	public void NewBall()
+	{
+		var random = new RandomNumberGenerator();
+		Vector2 position = Position;
+		position.X = win_size.X / 2;
+		random.Randomize();
+        position.Y = random.RandiRange(200, (int)win_size.Y - 200);
+        Position = position;
+		speed = StartSpeed;
+		// dir = RandomDirection();
+
+	}
+
+	public static Vector2 RandomDirection()
+	{
+		Random random = new Random();
+		Vector2 newDirection = new Vector2();
+		int[] choices = { 1, -1 };
+
+		newDirection.X = choices[random.Next(choices.Length)];
+		newDirection.Y = (float)(random.NextDouble() * 2 - 1);
+
+		return newDirection.Normalized();
+	}
 
     public override void _Process(double delta)
-    {
-        Position += _velocity * (float)delta;
-    }
+	{
+		//Vector2 motion = dir * speed * (float)delta
+		var collision = MoveAndCollide(dir * speed * (float)delta);
+		GodotObject collider;
 
-    private void StartMovement()
-    {
-        Random random = new Random();
-        double angle = random.NextDouble() * Math.PI * 2;
-        _velocity = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle)) * Speed;
-    }
+		if (collision!=null)
+		{
+			collider = collision.GetCollider();
+		}
+	}
 
-    public void Start()
-    {
-        Show();
-        GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
-        StartMovement();
-    }
+	private void StartMovement()
+	{
+		Random random = new Random();
+		double angle = random.NextDouble() * Math.PI * 2;
+		_velocity = new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle)) * Speed;
+	}
 
-    public void HandleCollision()
-    {
-        _velocity.X = -_velocity.X;
-    }
+	public void Start()
+	{
+		Show();
+		GetNode<CollisionShape2D>("CollisionShape2D").Disabled = false;
+		StartMovement();
+	}
+
+	public void HandleCollision()
+	{
+		_velocity.X = -_velocity.X;
+	}
 }
