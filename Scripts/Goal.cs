778d10fb4af4183bd54842c7f5b813306b1b9c2c
Cameron Chalupa <105525605+camchal@users.noreply.github.com>
2024-06-22 10:41:49 -0500

    Reset ball after scoring (#33)
    * Ball resets after being scored
    
    * ball resets

---
+++ Scripts/Goal.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+public partial class Goal : Node2D
+{
+	public int goalNumber{get; set;}
+	
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		//..this could probably be done better :)
+		goalNumber = (Name == "Goal1") ? 1 : 2; //used to know who scored eventually
+		
+	}
+
+	// Called every frame 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+
+	}
+
+
+	private async void OnBodyEntered(Node2D body)
+	{
+		if (body is Ball)
+		{
+			var ball = GetNode<Ball>("../Ball"); //maybe we can declare this earlier?
+			if (ball != null)
+			{
+				await ToSignal(GetTree().CreateTimer(1.5), "timeout");
+				ball.NewBall();
+			}
+			
+		}
+	}
+}
